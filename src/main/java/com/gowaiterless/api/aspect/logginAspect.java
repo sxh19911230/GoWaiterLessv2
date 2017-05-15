46d87991eacbb449d4cef6d0dfa4cbe435facf87
Forrest Sun <sxh19911230@gmail.com>
2017-05-15 01:24:37 -0700

    create loggin stub for later authorization (ex: only admin can create menu)

--- src/main/java/com/gowaiterless/api/aspect/logginAspect.java
+++ src/main/java/com/gowaiterless/api/aspect/logginAspect.java
@@ -2,13 +2,20 @@ package com.gowaiterless.api.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class logginAspect {
 	
-	@Before("within(com.gowaiterless.api.service.*)")
+	@Before("allServiceMethod() && !allServiceGetters()")
 	public void loggingAdvice(){
-		System.out.println("loggingAdvice Get Called");
+		System.out.println("Stub for loggin");
 	}
+	
+	@Pointcut("within(com.gowaiterless.api.service.*)")
+	public void allServiceMethod(){}
+	
+	@Pointcut("execution(public * com.gowaiterless.api.service.*.get*(..))")
+	public void allServiceGetters(){}
 
 }
