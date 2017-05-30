5ba032fd93a497699bb7b69d2b385cabe97ef3f9
Forrest Sun <sxh19911230@gmail.com>
2017-05-30 01:52:34 -0700

    remove some useless stuff

--- src/main/java/com/gowaiterless/api/controller/UserController.java
+++ src/main/java/com/gowaiterless/api/controller/UserController.java
@@ -1,19 +1,16 @@
 package com.gowaiterless.api.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.gowaiterless.api.User;
 
 @RestController
 @RequestMapping("user/")
 public class UserController {
 	@RequestMapping("me")
-	User getMe(HttpServletRequest res) {
-		User u = new User(res.getUserPrincipal().getName());
-		return u;
+	Principal getMe(Principal user) {
+		return user;
 	}
 
 }
