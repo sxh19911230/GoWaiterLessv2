98f280c7965c43fd2b628c9368e56b3daf6c643e
Forrest Sun <sxh19911230@gmail.com>
2018-01-04 01:54:16 -0800

    updated some test

--- src/test/java/waiterless/ApiTest.java
+++ src/test/java/waiterless/ApiTest.java
@@ -1,15 +1,46 @@
 package waiterless;
 
 import static com.jayway.restassured.RestAssured.given;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.jayway.restassured.RestAssured;
+
 public class ApiTest {
+
+	@BeforeClass
+    public static void setup() {
+        String port = System.getProperty("server.port");
+        if (port == null) {
+            RestAssured.port = Integer.valueOf(8080);
+        }
+        else{
+            RestAssured.port = Integer.valueOf(port);
+        }
+
+
+        String basePath = System.getProperty("server.base");
+        if(basePath==null){
+            basePath = "/api/";
+        }
+        RestAssured.basePath = basePath;
+
+        String baseHost = System.getProperty("server.host");
+        if(baseHost==null){
+            baseHost = "http://localhost";
+        }
+        RestAssured.baseURI = baseHost;
+
+    }
+	
 
 
 
     @Test
     public void testUserFetchesSuccess() {
-    	given().when().get("http://localhost:8080/api/v1/restaurant")
+    	given().when().get("/v1/restaurant")
         .then()
         .statusCode(200);
     }
